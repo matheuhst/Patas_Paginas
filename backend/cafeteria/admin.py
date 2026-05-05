8f09bc90ade8e7e10a704ee954682211f754d7bf
Eduardo <20dubu06@gmail.com>
2026-05-05 16:49:28 -0300

    Arrumando o admin de cada app e do core, para que seja possível manusear no servidor local e adicionar alguns produtos

--- backend/cafeteria/admin.py
+++ backend/cafeteria/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import (CafeInsumo, ReceitaDiretaAoCliente, ReceitaItem  )
 
-# Register your models here.
+admin.site.register(CafeInsumo)
+admin.site.register(ReceitaDiretaAoCliente)
+admin.site.register(ReceitaItem)
